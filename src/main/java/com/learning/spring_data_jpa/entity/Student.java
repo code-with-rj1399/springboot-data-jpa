7432f2c6f49d3116baa0d2142fb5e93d87880a2b
Rajesh <rajesh4.ramesh@gmail.com>
2025-03-25 23:02:57 +0530

    Mapping Entities with DB Different JPA Annotations

---
+++ src/main/java/com/learning/spring_data_jpa/entity/Student.java
@@ -0,0 +1,43 @@
+package com.learning.spring_data_jpa.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+@Table(
+        uniqueConstraints = @UniqueConstraint(
+                name = "emailid_unique",
+                columnNames = "email_address"
+        )
+)
+public class Student {
+
+    @Id
+    @SequenceGenerator(
+            name="student_sequence",
+            sequenceName="student_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "student_sequence"
+    )
+    private Long studentId;
+    private String firstName;
+    private String lastName;
+
+    @Email
+    @Column(
+            name = "email_address",
+            nullable = false
+    )
+    private String emailId;
+}
