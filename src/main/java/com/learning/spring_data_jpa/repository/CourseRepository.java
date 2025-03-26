5088a917cd6718861c95784d727e944eb43700ba
Rajesh <rajesh4.ramesh@gmail.com>
2025-03-26 12:34:49 +0530

    Paging and sorting

--- src/main/java/com/learning/spring_data_jpa/repository/CourseRepository.java
+++ src/main/java/com/learning/spring_data_jpa/repository/CourseRepository.java
@@ -1,9 +1,14 @@
 package com.learning.spring_data_jpa.repository;
 
 import com.learning.spring_data_jpa.entity.Course;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CourseRepository extends JpaRepository<Course, Long> {
+
+    List<Course> findByTitleContaining(String string, PageRequest pageRequest);
 }
--- src/test/java/com/learning/spring_data_jpa/repository/CourseRepositoryTest.java
+++ src/test/java/com/learning/spring_data_jpa/repository/CourseRepositoryTest.java
@@ -8,7 +8,11 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 
+import javax.swing.plaf.synth.SynthUI;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -45,6 +49,42 @@ class CourseRepositoryTest {
         List<Course> courses = courseRepository.findAll();
         courses.forEach(System.out::println);
     }
+
+
+    @Test
+    public void findAllPagination(){
+        PageRequest firstPageWith3Records = PageRequest.of(0, 3);
+        PageRequest secondPageWith2Records = PageRequest.of(1, 2);
+
+        Page<Course> page = courseRepository.findAll(firstPageWith3Records);
+        List<Course> content = page.getContent();
+        content.forEach(System.out::println);
+
+        page = courseRepository.findAll(secondPageWith2Records);
+        content = page.getContent();
+        content.forEach(System.out::println);
+    }
+
+    @Test
+    public void findAllBySorting(){
+        PageRequest firstPageWith3RecordsWithSorting = PageRequest.of(
+                0,
+                3,
+                Sort.by("credit").descending()
+                );
+
+        Page<Course> page = courseRepository.findAll(firstPageWith3RecordsWithSorting);
+        List<Course> content = page.getContent();
+        content.forEach(System.out::println);
+    }
+
+    @Test
+    public void findByTitleContainingAndSorting(){
+
+        List<Course> courses = courseRepository.findByTitleContaining("credit",PageRequest.of(1,2));
+        courses.forEach(System.out::println);
+    }
+
 
 
 }
